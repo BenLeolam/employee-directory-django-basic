79ca3acd0860f76a529a13470066fa8a46e7f8d6
BenLeolam <lovemebench@gmail.com>
2023-11-07 21:57:35 -0500

    complete employee directory

---
+++ employees/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name  = models.CharField(max_length=20)
+    last_name   = models.CharField(max_length=20)
+    photo       = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name 
